123da13f40b7930a0521ac4ca35c439f5d1d4eb9
Fancyer <254593558@qq.com>
2019-06-01 15:08:03 +0800

    年月日时分秒可写

--- date2.js
+++ date2.js
@@ -1,62 +1,114 @@
 class Date2 {
-  constructor () {
-    this._now = new Date()
+  constructor() {
+    const _d = new Date()
+    this._year = _d.getFullYear()
+    this._month = _d.getMonth() + 1
+    this._day = _d.getDate()
+    this._hour = _d.getHours()
+    this._minute = _d.getMinutes()
+    this._second = _d.getSeconds()
+
+    this.validators = {
+      y: (v) => {
+        const max = this.year + 100
+        if (typeof (v) !== 'number' || v > max || v < 1970) {
+          throw new Error(`Invalid year, must be a number between 1970 ~ ${max}`)
+        }
+      },
+      m(v) {
+        if (typeof (v) !== 'number' || v > 12 || v < 1) {
+          throw new Error('Invalid month, must be a number between 1 ~ 12')
+        }
+      },
+      d: (v) => {
+        const max = new Date(this.year, this.month, 0).getDate()
+        if (typeof (v) !== 'number' || v > max || v < 1) {
+          throw new Error(`Invalid date, must be a number between 1 ~ ${max}`)
+        }
+      },
+      h(v) {
+        if (typeof (v) !== 'number' || v > 23 || v < 0) {
+          throw new Error('Invalid hour, must be a number between 0 ~ 23')
+        }
+      },
+      s(v) {
+        if (typeof (v) !== 'number' || v > 59 || v < 0) {
+          throw new Error('Invalid value, must be a number between 0 ~ 59')
+        }
+      },
+    }
   }
 
-  toString (fmt) {
-    // todo: 添加自定义格式的功能
-    return `${this.year()}-${this.months()}-${this.days()} ${this.hours()}:${this.minutes()}:${this.seconds()}`
+  get year() {
+    return this._year
   }
 
-  year () {
-    return this._now.getFullYear()
+  set year(v) {
+    this.validators.y(v)
+    this._year = v
   }
 
-  month () {
-    return this._now.getMonth() + 1
+  get month() {
+    return this._month
   }
 
-  months () {
-    return this._pad(this.month())
+  set month(v) {
+    this.validators.m(v)
+    this._month = v
   }
 
-  day () {
-    return this._now.getDate()
+  get day() {
+    return this._day
   }
 
-  days () {
-    return this._pad(this.day())
+  set day(v) {
+    this.validators.d(v)
+    this._day = v
   }
 
-  dayOfWeek () {
-    return this._now.getDay()
+  get hour() {
+    return this._hour
   }
 
-  hour () {
-    return this._now.getHours()
+  set hour(v) {
+    this.validators.h(v)
+    this._hour = v
   }
 
-  hours () {
-    return this._pad(this.hour())
+  get minute() {
+    return this._minute
   }
 
-  minute () {
-    return this._now.getMinutes()
+  set minute(v) {
+    this.validators.s(v)
+    this._minute = v
   }
 
-  minutes () {
-    return this._pad(this.minute())
+  get second() {
+    return this._second
   }
 
-  second () {
-    return this._now.getSeconds()
+  set second(v) {
+    this.validators.s(v)
+    this._second = v
   }
 
-  seconds () {
-    return this._pad(this.second())
+  get dayOfWeek() {
+    // use user-set date to get the day of week
+    // use '/' instead of '-' to avoid bug
+    return new Date(`${this.year}/${this.month}/${this.day}`).getDay()
   }
 
-  _pad (v) {
+  // WebStorm suggests to static it.
+  // 为什么需要有静态方法?益处是什么？
+  // 静态方法为什么在constructor上?
+  static _pad(v) {
     return v > 9 ? v : `0${v}`
+  }
+
+  // todo: custom format
+  toString(fmt) {
+    const [mo, d, h, mi, s] = [this.constructor._pad(this.month), this.constructor._pad(this.day), this.constructor._pad(this.hour), this.constructor._pad(this.minute), this.constructor._pad(this.second)]
+    return `${this.year}-${mo}-${d} ${h}:${mi}:${s}`
   }
 }
