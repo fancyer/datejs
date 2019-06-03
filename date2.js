6acbc035469bc064801e47fd67537f8a15e29662
侯胜楠 <seavin@163.com>
2019-06-03 18:19:18 +0800

    继承原生Date 添加add方法

--- date2.js
+++ date2.js
@@ -1,113 +1,105 @@
-class Date2 {
-  constructor() {
-    const _d = new Date()
-    this._year = _d.getFullYear()
-    this._month = _d.getMonth() + 1
-    this._day = _d.getDate()
-    this._hour = _d.getHours()
-    this._minute = _d.getMinutes()
-    this._second = _d.getSeconds()
-
-    this.validators = {
-      y: (v) => {
-        const max = this.year + 100
-        if (typeof (v) !== 'number' || v > max || v < 1970) {
-          throw new Error(`Invalid year, must be a number between 1970 ~ ${max}`)
-        }
-      },
-      m(v) {
-        if (typeof (v) !== 'number' || v > 12 || v < 1) {
-          throw new Error('Invalid month, must be a number between 1 ~ 12')
-        }
-      },
-      d: (v) => {
-        const max = new Date(this.year, this.month, 0).getDate()
-        if (typeof (v) !== 'number' || v > max || v < 1) {
-          throw new Error(`Invalid date, must be a number between 1 ~ ${max}`)
-        }
-      },
-      h(v) {
-        if (typeof (v) !== 'number' || v > 23 || v < 0) {
-          throw new Error('Invalid hour, must be a number between 0 ~ 23')
-        }
-      },
-      s(v) {
-        if (typeof (v) !== 'number' || v > 59 || v < 0) {
-          throw new Error('Invalid value, must be a number between 0 ~ 59')
-        }
-      },
-    }
+/**
+ * @description 增强原生Date
+ * @constructor
+ * @params 可选参数与原生Date相同
+ * eg:
+ * new Date2()
+ * new Date2(12345)
+ * new Date('2011/11/11')
+ * new Date(year, monthIndex [, day [, hours [, minutes [, seconds [, milliseconds]]]]])
+ */
+class Date2 extends Date{
+  constructor (...args) {
+    super()
+    this._d = new Date(...args)
   }
 
-  get year() {
-    return this._year
+  get year () {
+    return this._d.getFullYear()
   }
 
-  set year(v) {
-    this.validators.y(v)
-    this._year = v
+  set year (v) {
+    this._d = new Date(v, this.month, this.day, this.hour, this.minute, this.second)
   }
 
-  get month() {
-    return this._month
+  get month () {
+    return this._d.getMonth() + 1
   }
 
-  set month(v) {
-    this.validators.m(v)
-    this._month = v
+  set month (v) {
+    // handle monthIndex
+    this._d = new Date(this.year, v - 1, this.day, this.hour, this.minute, this.second)
   }
 
-  get day() {
-    return this._day
+  get day () {
+    return this._d.getDate()
   }
 
-  set day(v) {
-    this.validators.d(v)
-    this._day = v
+  set day (v) {
+    this._d = new Date(this.year, this.month - 1, v, this.hour, this.minute, this.second)
   }
 
-  get hour() {
-    return this._hour
+  get hour () {
+    return this._d.getHours()
   }
 
-  set hour(v) {
-    this.validators.h(v)
-    this._hour = v
+  set hour (v) {
+    this._d = new Date(this.year, this.month - 1, this.day, v, this.minute, this.second)
   }
 
-  get minute() {
-    return this._minute
+  get minute () {
+    return this._d.getMinutes()
   }
 
-  set minute(v) {
-    this.validators.s(v)
-    this._minute = v
+  set minute (v) {
+    this._d = new Date(this.year, this.month - 1, this.day, this.hour, v, this.second)
   }
 
-  get second() {
-    return this._second
+  get second () {
+    return this._d.getSeconds()
   }
 
-  set second(v) {
-    this.validators.s(v)
-    this._second = v
+  set second (v) {
+    this._d = new Date(this.year, this.month - 1, this.day, this.hour, this.minute, v)
   }
 
-  get dayOfWeek() {
+  get dayOfWeek () {
     // use user-set date to get the day of week
     // use '/' instead of '-' to avoid bug
-    return new Date(`${this.year}/${this.month}/${this.day}`).getDay()
+    return this._d.getDay()
   }
 
   // WebStorm suggests to static it.
   // 为什么需要有静态方法?益处是什么？
   // 静态方法为什么在constructor上?
-  static _pad(v) {
+  static _pad (v) {
     return v > 9 ? v : `0${v}`
   }
 
+  /**
+   * 在当前date基础上加上一个数值，返回新的Date2实例
+   * @param {number} value - 要增加的数值
+   * @param {string} label - 要增加的字段, 可选值为 year/month/day/hour/minute/second
+   * @returns {Date2}      - 新的 Date2 实例
+   */
+  add (value, label) {
+    if (typeof value !== 'number') {
+      throw new Error('Invalid value, must be a number')
+    }
+    if (['year', 'month', 'day', 'hour', 'minute', 'second'].includes(label) === false) {
+      throw new Error('Invalid label')
+    }
+    // handle monthIndex
+    const params = [this.year, this.month - 1, this.day, this.hour, this.minute, this.second]
+
+    // return new instance
+    let ins = new Date2(...params)
+    ins[label] += value
+    return ins
+  }
+
   // todo: custom format
-  toString(fmt) {
+  toString (fmt) {
     const [mo, d, h, mi, s] = [this.constructor._pad(this.month), this.constructor._pad(this.day), this.constructor._pad(this.hour), this.constructor._pad(this.minute), this.constructor._pad(this.second)]
     return `${this.year}-${mo}-${d} ${h}:${mi}:${s}`
   }
